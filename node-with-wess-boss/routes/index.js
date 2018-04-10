170224996c6f5c938026bcec85609dbea8dfcbdc
mardiniii <sezama03@hotmail.com>
2018-04-10 10:09:18 -0700

    Query stores from database

--- node-with-wess-boss/routes/index.js
+++ node-with-wess-boss/routes/index.js
@@ -4,7 +4,8 @@ const storeController = require('../controllers/storeController');
 // Just to import one method from the imported file
 const { catchErrors } = require('../handlers/errorHandlers')
 // Do work here
-router.get('/', storeController.homePage);
+router.get('/', storeController.getStores);
+router.get('/stores', storeController.getStores);
 router.get('/add', storeController.addStore);
 // Async/Await with function composition for errors catching
 router.post('/add', catchErrors(storeController.createStore));
