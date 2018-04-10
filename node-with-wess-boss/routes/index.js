34f416ae85a48e520830a3f14623729f57aed96d
mardiniii <sezama03@hotmail.com>
2018-04-09 18:49:08 -0600

    Create store model

--- node-with-wess-boss/routes/index.js
+++ node-with-wess-boss/routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const wes = { name: 'Sebas', age: 28, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(wes);
+  res.render('index');
 });
 
 module.exports = router;
