dcae006244bcefa94ddca36fb3d7f905202e002c
mardiniii <sezama03@hotmail.com>
2018-04-10 08:05:26 -0600

    Using async/await to store in the database

--- node-with-wess-boss/routes/index.js
+++ node-with-wess-boss/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
-
+// Just to import one method from the imported file
+const { catchErrors } = require('../handlers/errorHandlers')
 // Do work here
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+// Async/Await with function composition for errors catching
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
