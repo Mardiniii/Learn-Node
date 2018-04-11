2bba0f08af62aece36cfb0e598954e60f67b9c18
mardiniii <sezama03@hotmail.com>
2018-04-11 16:57:53 -0500

    Add feature to update email and name

--- node-with-wess-boss/routes/index.js
+++ node-with-wess-boss/routes/index.js
@@ -38,7 +38,8 @@ router.post('/register',
   userController.register,
   authController.login
 );
-
 router.get('/logout', authController.logout)
+router.get('/account', authController.isLoggedIn, userController.account)
+router.post('/account', catchErrors(userController.updateAccount));
 
 module.exports = router;
