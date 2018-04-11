79c75d37de37516b251d489866ad0bb2c2521da3
mardiniii <sezama03@hotmail.com>
2018-04-11 10:58:15 -0500

    Add edit, update, images uploading and single store view

--- node-with-wess-boss/routes/index.js
+++ node-with-wess-boss/routes/index.js
@@ -4,10 +4,23 @@ const storeController = require('../controllers/storeController');
 // Just to import one method from the imported file
 const { catchErrors } = require('../handlers/errorHandlers')
 // Do work here
-router.get('/', storeController.getStores);
-router.get('/stores', storeController.getStores);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 // Async/Await with function composition for errors catching
-router.post('/add', catchErrors(storeController.createStore));
+router.post('/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore)
+);
+
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.post('/add/:id',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore)
+);
+
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 module.exports = router;
