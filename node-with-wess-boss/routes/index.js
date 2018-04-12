6b485cc0381c787b535e707ffa146d6a954cf962
mardiniii <sezama03@hotmail.com>
2018-04-12 11:30:56 -0500

    Create Top stores pages based on reviews using advanced aggregations

--- node-with-wess-boss/routes/index.js
+++ node-with-wess-boss/routes/index.js
@@ -54,7 +54,7 @@ router.post('/reviews/:id',
   authController.isLoggedIn,
   catchErrors(reviewController.addReview)
 );
-
+router.get('/top', catchErrors(storeController.getTopStores))
 /*
   API
 */
